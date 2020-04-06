7c85b7be477304e14de4d2ca61f0de8913e9c540
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-04-06 13:16:14 +0200

    Change className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ import '../stylesheets/Footer.scss';
 function Footer(props) {
   return (
     <footer>
-      <div class='footer'>
-        <p class='footer__text'>Awesome profile-cards @2020</p>
-        <img class='footer__logo' src={logoAdalab} title='Logo Adalab' alt='Logo de Adalab' />
+      <div className="footer">
+        <p className="footer__text">Awesome profile-cards @2020</p>
+        <img className="footer__logo" src={logoAdalab} title="Logo Adalab" alt="Logo de Adalab" />
       </div>
     </footer>
   );
