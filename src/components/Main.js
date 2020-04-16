9fb564c96de56c3f5b020d4ef021c2453bb14d4d
jemrosas <jmezrosas@gmail.com>
2020-04-16 18:51:03 +0200

    Add minor changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,27 +4,10 @@ import Box2 from './Box2';
 import '../stylesheets/Main.scss';
 import { useState } from 'react';
 
-//  let formObject = {
-//   name: 'pipi',
-//   job: 'Front End Developer',
-//   email: 'pipi@gmail.com',
-//   tel: '644555334',
-//   linkedin: 'https://www.linkedin.com/in/beatrizfleon/',
-//   github: 'https://github.com/Beatrizfleon',
-// };
-
 function Main(props) {
   const [data, setData] = useState({ palette: 'green' });
 
   localStorage.setItem('userInfo', JSON.stringify(data));
-
-  // const handleInputPreview = (value) => {
-  //   const newData = {
-  //     ...data,
-  //     [value.name]: value.value,
-  //   };
-  //   setData(newData);
-  // };
 
   const handleUserInfo = (value) => {
     const newData = {
