66e551a15769e2f1389677b5dec87cf45611e128
Beatrizfleon <Beatrizfleon@gmail.com>
2020-04-18 21:25:12 +0200

    added a loader just after clicking on create card

--- src/components/Share.js
+++ src/components/Share.js
@@ -5,26 +5,33 @@ import fetchUrl from '../services/fetchUrl';
 
 function Share(props) {
   const [url, setUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleCreateCard = (info) => {
+    //mostrar el spinner
+    setIsLoading(true);
     fetchUrl(info)
       .then(function (resp) {
         return resp.json();
       })
       .then(function (result) {
         setUrl(result.cardURL);
+        //ocultar el spinner
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.log(error);
       });
   };
-
+  let loaderStyle = isLoading ? '' : 'hidden';
   return (
     <div className='share__button share__button js-share__button'>
       <button className='share__button__enabled js-button' onClick={() => handleCreateCard(props.stateInfo)}>
         <i className='far fa-address-card'></i>
         <h2 className='share__button__title'>Crear tarjeta</h2>
       </button>
+      <div class={`loader ${loaderStyle}`}></div>
+
       <ShareCard isShowing={url !== ''} cardURL={url} />
     </div>
   );
