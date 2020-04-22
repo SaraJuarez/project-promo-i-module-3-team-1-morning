7395be7c8c6868b7a74614161477064f9a33fcc6
Rosanna Blanco <rosanna.blanco.blanco@gmail.com>
2020-04-22 08:53:36 +0200

    Add folder docs

---
+++ docs/precache-manifest.fae887f985c72a26a1b9f1c603da9b3b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1181d24dd9ab53c4554451e8c9bed1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e5d5dbbd3a6856fa6f0",
+    "url": "./static/css/main.1c0d9733.chunk.css"
+  },
+  {
+    "revision": "b0c63237b2f55af84202",
+    "url": "./static/js/2.d71a472c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.d71a472c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e5d5dbbd3a6856fa6f0",
+    "url": "./static/js/main.0d71e723.chunk.js"
+  },
+  {
+    "revision": "ef0e3c2bbbad658ef01a",
+    "url": "./static/js/runtime-main.4c2e7753.js"
+  },
+  {
+    "revision": "d00353c39437015b729219d44a61826f",
+    "url": "./static/media/image-rogelia.d00353c3.jpg"
+  },
+  {
+    "revision": "513646c97a2ff77a3c97208fe94916de",
+    "url": "./static/media/keyboard-regular.513646c9.svg"
+  },
+  {
+    "revision": "c0cdd664b1f2b8b65c9cd9b5e59bfc94",
+    "url": "./static/media/object-ungroup-regular.c0cdd664.svg"
+  },
+  {
+    "revision": "6c17896f7f4aab3a519a3a52efa2c490",
+    "url": "./static/media/share-alt-solid.6c17896f.svg"
+  },
+  {
+    "revision": "f5d4be6ddd2d27eb9413e9e417375077",
+    "url": "./static/media/tarjetas-molonas.f5d4be6d.png"
+  }
+]);
